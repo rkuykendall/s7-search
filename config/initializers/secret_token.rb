1d5689fbf8c320974ea369c1af58bcd975e357f6
Robert Kuykendall <robert@rkuykendall.com>
2012-04-21 16:03:59 -0500

    Initial commit of project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S7search::Application.config.secret_token = 'e50d47ade68d5effa0a2c864943e6e9ea7cddaaa00b2cef3808be8804b25efe9bf5e5c18381e35f2ab6f0a0ffb771d4121266e701e9feb2cac3c4eea3e1133d5'
